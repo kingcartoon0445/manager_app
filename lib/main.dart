ae922943a69ecaab5b6005e48a686fc3b1209d5f
kingcartoon0445 <dauxanh0445@gmail.com>
2024-09-11 14:22:15 +0700

    add muti language

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
-
+import 'package:easy_localization/easy_localization.dart';
 export 'package:manager_app/config/app_config.dart';
-export 'package:manager_app/routes/route_names.dart'; 
+export 'package:manager_app/routes/route_names.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:manager_app/bloc/counter/counter_bloc.dart';
-import 'config/app_config.dart'; 
+import 'config/app_config.dart';
 import 'routes/app_routes.dart';
 import 'routes/route_names.dart';
 
 void main() {
-  AppConfig.init(environment: 'dev');  // Khởi tạo cấu hình cho môi trường
+  AppConfig.init(environment: 'dev'); // Khởi tạo cấu hình cho môi trường
 
-  runApp(MyApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: [const Locale('en', 'US'), const Locale('vi', 'VN')],
+      path: 'assets/langs', // Đường dẫn đến thư mục chứa file JSON
+      fallbackLocale: const Locale('en', 'US'),
+      startLocale: const Locale('vi', 'VN'),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,9 +31,13 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => CounterBloc()), // Đăng ký CounterBloc
       ],
       child: MaterialApp(
-        title: AppConfig.instance.appName,  // Sử dụng AppConfig
-        initialRoute: RouteNames.home,      // Đặt route khởi đầu
-        onGenerateRoute: AppRoutes.generateRoute, // Đăng ký hệ thống quản lý route
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        title: AppConfig.instance.appName, // Sử dụng AppConfig
+        initialRoute: RouteNames.home, // Đặt route khởi đầu
+        onGenerateRoute:
+            AppRoutes.generateRoute, // Đăng ký hệ thống quản lý route
         debugShowCheckedModeBanner: false,
       ),
     );
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 import '../../main.dart'; 
@@ -7,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text('welcome').tr(),
       ),
       body: Center(
         child: Column(
